1723af2a0649435ee013d9083e6e022a360753af
Hackbright Student <info@hackbrightacademy.com>
2016-04-06 12:14:15 -0700

    pow function corrected

--- arithmetic.py
+++ arithmetic.py
@@ -1,28 +1,41 @@
 def add(num1, num2):
     """Returns the sum of the two input integers"""
+
     return num1 + num2
+
 
 def subtract(num1, num2):
     """Returns the second number subtracted from the first"""
+
     return num1 - num2
+
 
 def multiply(num1, num2):
     """Multiplies the two inputs together"""
+
     return num1 * num2
+
 
 def divide(num1, num2):
     """Divides the first input by the second, returning a floating point"""
+
     return num1 / num2
+
 
 def square(num1):
     """Returns the square of the input"""
+
     return num1 * num1
 
+
 def cube(num1):
+    """Returns the cube of the input"""
+
     return num1 * num1 * num1
 
+
 def power(num1, num2):
-    pass
+    return num1 ** num2
 
 def mod(num1, num2):
     pass
