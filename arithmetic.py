7933b01b029ccc3312227c29623fe727ccd387c3
Hackbright Student <info@hackbrightacademy.com>
2016-04-06 12:06:07 -0700

    subtract function corrected

--- arithmetic.py
+++ arithmetic.py
@@ -1,8 +1,9 @@
 def add(num1, num2):
+    """Returns the sum of the two input integers"""
     return num1 + num2
 
 def subtract(num1, num2):
-    pass
+    return num1 - num2
 
 def multiply(num1, num2):
     pass
